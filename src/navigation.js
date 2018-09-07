b7a7ac02b2632a6a446b73e302e79106e1d41062
blakemecham <bam.mecham@gmail.com>
2018-09-07 12:01:07 -0600

    added a few items for the nav bar and added the line of code for font awesome

---
+++ src/navigation.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const Navigation = function() {
+  return (
+    <div>
+      <div>
+          <div>
+            <i class="fab fa-facebook"></i>
+          </div>
+          <div>
+            <input type='text' />
+          </div>
+          <div>
+            <img />
+            name
+          </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navigation;
