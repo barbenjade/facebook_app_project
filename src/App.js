88b16d4391692617f6a4683b023109b0f91e18a5
Jade Barben <barbenjade>
2018-09-07 13:51:01 -0600

    first commit for side menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Menu from './menu.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-      <Menu />
+        <Menu />
       </div>
     );
   }
--- src/menu.js
+++ src/menu.js
@@ -1,33 +1,36 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
-
+import './menu.css';
 
 class Menu extends Component {
+  render () {
     return (
-      <div>
+      <div className="menu_main">
         <h3>Name</h3>
         <ul id="main_ul">
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+          <li>News Feed</li>
+          <li>Messenger</li>
+          <li>Watch</li>
+          <li>Marketplace</li>
         </ul>
         <h3>Shortcuts</h3>
         <ul id="shortcuts">
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+          <li>Group1</li>
+          <li>Group2</li>
+          <li>Group3</li>
+          <li>Group4</li>
         </ul>
         <h3>Explore</h3>
         <ul id="explore">
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+          <li>Events</li>
+          <li>Groups</li>
+          <li>Pages</li>
+          <li>Fundraisers</li>
+          <li>Friend List</li>
         </ul>
       </div>
     )
+  }
 }
 
 
