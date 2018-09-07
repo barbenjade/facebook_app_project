ba6ae177fa5b985002f2089d965755f52d85b42c
Jade Barben <barbenjade>
2018-09-07 15:32:24 -0600

    final sumbission

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import Menu from './menu.js';
 import './App.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 class App extends Component {
   render() {
--- src/menu.js
+++ src/menu.js
@@ -2,31 +2,34 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './menu.css';
 
+
 class Menu extends Component {
   render () {
     return (
       <div className="menu_main">
         <h3>Name</h3>
         <ul id="main_ul">
-          <li>News Feed</li>
-          <li>Messenger</li>
-          <li>Watch</li>
-          <li>Marketplace</li>
+          <li className="list-group-item"><i class="far fa-newspaper"></i>News Feed</li>
+          <li className="list-group-item"><i class="fab fa-facebook-messenger"></i>Messenger</li>
+          <li className="list-group-item"><i class="fab fa-youtube-square"></i>Watch</li>
+          <li className="list-group-item"><i class="fas fa-warehouse"></i>Marketplace</li>
         </ul>
         <h3>Shortcuts</h3>
         <ul id="shortcuts">
-          <li>Group1</li>
-          <li>Group2</li>
-          <li>Group3</li>
-          <li>Group4</li>
+          <li className="list-group-item"><i class="fas fa-user-friends"></i>Group1</li>
+          <li className="list-group-item"><i class="fas fa-user-friends"></i>Group2</li>
+          <li className="list-group-item"><i class="fas fa-layer-group"></i>Group3</li>
+          <li className="list-group-item"><i class="fas fa-layer-group"></i>Group4</li>
         </ul>
         <h3>Explore</h3>
         <ul id="explore">
-          <li>Events</li>
-          <li>Groups</li>
-          <li>Pages</li>
-          <li>Fundraisers</li>
-          <li>Friend List</li>
+          <li className="list-group-item"><i class="fas fa-calendar-alt"></i>Events</li>
+          <li className="list-group-item"><i class="fas fa-user-friends"></i>Groups</li>
+          <li className="list-group-item"><i class="fas fa-flag-checkered"></i>Pages</li>
+          <li className="list-group-item"><i class="fas fa-coins"></i>Fundraisers</li>
+          <li className="list-group-item"><i class="fas fa-user-alt"></i>
+
+Friend List</li>
         </ul>
       </div>
     )
