f1112bca3008fb777d563fb1d53cfa0efe85961e
blakemecham <bam.mecham@gmail.com>
2018-09-07 15:19:32 -0600

    styled navbar

--- src/navigation.js
+++ src/navigation.js
@@ -1,21 +1,41 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import './navigation.css'
 
 const Navigation = function() {
   return (
-    <div>
-      <div>
-          <div>
+    <div className='navwrapper'>
+        <div className='navwrapperleft'>
+          <div className='facebook'>
             <i class="fab fa-facebook"></i>
           </div>
-          <div>
+          <div className='textbox'>
             <input type='text' />
           </div>
-          <div>
-            <img />
-            name
+          <div className='name'>
+            <i class="fas fa-user-circle"></i>
+            Name
           </div>
-      </div>
+          <div className='home'>
+            Home
+          </div>
+        </div>
+        <div className='navwrapperright'>
+          <div className='friends'>
+            <i class="fas fa-user-friends"></i>
+          </div>
+          <div className='comments'>
+            <i class="fas fa-comments"></i>
+          </div>
+          <div className='world'>
+            <i class="fas fa-globe-americas"></i>
+          </div>
+          <div className='question'>
+            <i class="fas fa-question-circle"></i>
+          </div>
+          <div className='downarrow'>
+            <i class="fas fa-caret-down"></i>
+          </div>
+        </div>
     </div>
   );
 }
