1f4b92e56220c921e1f9c7cd5e93f3f25c2cbfc9
terenitup <terentaniuchi@gmail.com>
2018-09-07 13:22:18 -0600

    added a statusUpdate folder and added a makePost component as part of the statusUpdate bar in the fb app.

---
+++ src/statusUpdate/makePost.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class MakePost extends Component {
+  // constructor(props) {
+  //   super(props)
+  // }
+
+  render() {
+    return (
+      <div>
+        Make Post
+      </div>
+    );
+  }
+}
