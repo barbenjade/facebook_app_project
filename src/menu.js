2b87e8bceeebefe06649548090ec6e9bf61847ac
Jade Barben <barbenjade>
2018-09-07 11:55:09 -0600

    created menu div

---
+++ src/menu.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+
+
+class Menu extends Component {
+    return (
+      <div>
+        <h3>Name</h3>
+        <ul id="main_ul">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+        <h3>Shortcuts</h3>
+        <ul id="shortcuts">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+        <h3>Explore</h3>
+        <ul id="explore">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    )
+}
+
+
+export default Menu;
